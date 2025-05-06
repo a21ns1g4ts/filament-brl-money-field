0e144c67d887b1befd924870cf9e753d437058b7
a21ns1g4ts <atila.danvi@outlook.com>
2025-05-06 14:06:11 -0300

    Add BrlMoney component for formatted currency input

---
+++ src/Filament/Forms/Components/BrlMoney.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace A21ns1g4ts\FilamentBrlMoneyField\Forms\Components;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Support\RawJs;
+
+class BrlMoney extends TextInput
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->mask(RawJs::make(<<<'JS'
+                $input => {
+                    let value = $input.replace(/\D/g, '');
+                    value = (value / 100).toFixed(2) + '';
+                    value = value.replace('.', ',');
+                    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+                    return value;
+                }
+            JS))
+            ->dehydrateStateUsing(fn ($state) =>
+                $state ? str_replace(',', '.', str_replace('.', '', $state)) : null
+            )
+            ->inputMode('decimal');
+    }
+}
